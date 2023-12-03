53b63ba968ec70be971fc0397e4d9d0aaaef4738
bangrezz <rezaekafebrian40@gmail.com>
2023-12-03 11:22:50 +0800

    added ui to show format name file

---
+++ ui/selectFormatHostname.py
@@ -0,0 +1,14 @@
+def selectFormat():
+      print("""
+Welcome to the "name of project". The script will generate export or log file of MikroTik
+Select the format hostname :
+            
+            
+(1) ---> MikroTik-1_2023-11-20_10-00-12
+
+(2) ---> 192.168.1.1_2023-11-20_10-00-12
+
+(q) Exit the program
+
+Meaning the format name is = hostname or ip address_date_clock
+""")
