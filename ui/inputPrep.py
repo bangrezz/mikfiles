ea62b89029462267abadd00cc40936aa0dcbeb6f
bangrezz <rezaekafebrian40@gmail.com>
2023-12-05 11:08:10 +0800

    Changes banner definition

---
+++ ui/inputPrep.py
@@ -0,0 +1,32 @@
+import os
+cls = os.system('clear')
+
+class ExportFileDef:
+    def hostnameLogDef():
+        print("[->] Export Log File with Hostname format")
+    def ipAddrLogDef():
+        print("[->] Export Log File with IP Address format")
+    def hostnameFileConfDef():
+        print("[->] Export File Configuration and Backup file with Hostname format")
+    def ipAddrFileConfDef():
+        print("[->] Export File Configuration and Backup file with IP Address format")
+
+def hostnameDefSelection():
+    cls
+    print("[->] Export with Hostname format")
+def ipAddrDefSelection():
+    cls
+    print("[->] Export with IP address format")
+
+def inputPrep():
+    print("""
+        [*] Input range IP Address with format in below
+             
+        192.168.1.1 [single IP Address]
+            or
+        192.168.1.0/24
+            or
+        192.168.1.1-192.168.1.100 (no space between '-')
+
+
+        """)
---
+++ ui/selectFileExport.py
@@ -0,0 +1,17 @@
+def selectFile():
+    print("""
+Select to Export file
+            
+            
+(1) Export log file
+
+(2) Export file configuration and backup file
+
+(3) Export both
+
+(b) Back to the previous
+
+(q) Exit the program
+
+Meaning the format name is = hostname or ip address_date_clock
+""")
