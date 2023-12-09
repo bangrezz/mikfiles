f357c5ffc2a201869ec2f5d4e638c5846ffef89a
bangrezz <rezaekafebrian40@gmail.com>
2023-12-09 11:26:16 +0800

    Implemented mikfiles lite new.py

--- modules/HostnameExBoth.py
+++ modules/HostnameExBoth.py
@@ -9,11 +9,11 @@ import re
 
 os.system('')
 
-def login_mikrotik(ip, username, password):
+def login_mikrotik(ip, username, password, port):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        ssh.connect(ip, username=username, password=password, timeout=20)
+        ssh.connect(ip, port=port, username=username, password=password, timeout=3)
         print("\033[32m" + f"[+] Berhasil login ke {ip}" + "\033[0m")
 
         # Get the hostname
@@ -27,9 +27,8 @@ def login_mikrotik(ip, username, password):
         # Define the log filename
         log_filename = f"{hostname}_{date_time}"
 
-        # Export the file conf and backup file
-        command = f"/log print file={log_filename}; /system backup save name={log_filename}; /export file={log_filename}"
-        #command = f"/export file={log_filename}"
+        # Export the log file
+        command = f"/log print file={log_filename};/system backup save name={log_filename}; /export file={log_filename}"
         stdin, stdout, stderr = ssh.exec_command(command)
         print(f"[*] Eksekusi perintah '{command}' di {ip}:")
         print(stdout.read().decode())
@@ -41,7 +40,7 @@ def login_mikrotik(ip, username, password):
 
         # Pola regexp untuk mencocokkan nama file
         # \S+ cocok dengan satu atau lebih karakter non-whitespace
-        pattern = r"\b\S+_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.(txt|rsc|backup)\b"
+        pattern = r"\b\S+_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.(txt|backup|rsc)\b"
 
         # Mendapatkan daftar semua file di direktori root
         files = sftp.listdir('/')
@@ -63,33 +62,58 @@ def login_mikrotik(ip, username, password):
 
     except socket.timeout:
         print("\033[31m" + f"[!] Target {ip} tidak ada: Connection timed out." + "\033[0m")
-        print("[*] Coba lagi menggunakan network atau IP address yang berbeda")
-        sys.exit(1)
+        print("[*] Maybe devices is off or filtered by firewall devices")
     except paramiko.AuthenticationException:
         print(f"[!] Gagal login ke {ip} authentication failed")
     except Exception as e:
         print(f"[*] Gagal login ke {ip}: {e}")
     finally:
         ssh.close()
-
+            
 def main():
     os.system('clear')
     from ui import inputPrep
     inputPrep.hostnameDefSelection()
     inputPrep.ExportFileDef.hostnameExportBoth()
     inputPrep.inputPrep()
-    ip_range_input = input("IP Address : ")
     username = str(input("Input Username : "))
     password = str(input("Input Password : "))
-    if '-' in ip_range_input:
-        start_ip, end_ip = ip_range_input.split('-')
-        start_ip = ipaddress.IPv4Address(start_ip)
-        end_ip = ipaddress.IPv4Address(end_ip)
+    port = 22  # Default port value
+    
+    def portInput():
+        nonlocal port
+        port_input = input("Masukkan port SSH [Press Enter if default (port 22)] : ")
+        if port_input:
+            try:
+                port = int(port_input)
+            except ValueError:
+                print("\n [!] Port harus berupa angka.")
+                portInput()
+    portInput()
 
-        for ip_int in range(int(start_ip), int(end_ip) + 1):
-            ip = ipaddress.IPv4Address(ip_int)
-            login_mikrotik(str(ip), username, password)
-    else:
-        ip_range = ipaddress.ip_network(ip_range_input)
-        for ip in ip_range.hosts():
-            login_mikrotik(str(ip), username, password)
+    def AddrInput():
+        ip_input = input("input IP Address : ")
+
+        # Memisahkan input berdasarkan koma dan menghapus spasi
+        ip_entries = [ip.strip() for ip in ip_input.split(',')]
+        for entry in ip_entries:
+            if '-' in entry:
+                start_ip, end_ip = entry.split('-')
+                start_ip = ipaddress.IPv4Address(start_ip)
+                end_ip = ipaddress.IPv4Address(end_ip)
+
+                for ip_int in range(int(start_ip), int(end_ip) + 1):
+                    ip = ipaddress.IPv4Address(ip_int)
+                    login_mikrotik(str(ip), username, password, port)
+            elif '/' in entry:
+                ip_range = ipaddress.ip_network(entry, strict=False)
+                for ip in ip_range.hosts():
+                    login_mikrotik(str(ip), username, password, port)
+            else:
+                try:
+                    ipaddress.IPv4Address(entry)  # Memvalidasi alamat IP
+                    login_mikrotik(entry, username, password, port)
+                except ipaddress.AddressValueError as ave:
+                    print(f"Alamat IP tidak valid: {ave}")
+                    AddrInput()
+    AddrInput()
