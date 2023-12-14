aed23e454b8f2b9b962bd13053f6a81b4b4999eb
bangrezz <rezaekafebrian40@gmail.com>
2023-12-14 10:55:23 +0800

    function to add cron config

--- modules/cronDev/crondevWithmenu.py
+++ modules/cronDev/crondevWithmenu.py
@@ -34,33 +34,6 @@ def cek_sistem_dan_file():
     return True
 
 # Fungsi untuk menganalisis dan menjelaskan baris konfigurasi cron
-"""
-def jelaskan_cron():
-    try:
-        with open('/etc/crontab', 'r') as file:
-            # Menambahkan variabel penghitung
-            counter = 1
-            for line in file:
-                # Memeriksa apakah baris tidak dimulai dengan "#" dan bukan baris kosong
-                if not line.strip().startswith('#') and line.strip():
-                    # Menampilkan konfigurasi cron asli
-                    print(f"Konfigurasi Cron Asli {counter}: {line.strip()}")
-                    parts = line.split()
-                    if len(parts) >= 6:
-                        # Mengganti karakter '*' dengan '-' dalam bagian waktu
-                        waktu = ' '.join(part if part != '*' else '-' for part in parts[:5])
-                        # Menjelaskan waktu jadwal cron
-                        waktu_jelaskan = f"Menit: {parts[0].replace('*', '-')}, Jam: {parts[1].replace('*', '-')}, Hari dari bulan: {parts[2].replace('*', '-')}, Bulan: {parts[3].replace('*', '-')}, Hari dari minggu: {parts[4].replace('*', '-')}"
-                        # Menjelaskan perintah yang akan dijalankan
-                        perintah = ' '.join(parts[5:])
-                        print(f"{counter}. {waktu_jelaskan}, Perintah: {perintah}")
-                        # Meningkatkan penghitung
-                        counter += 1
-                    else:
-                        print("Baris tidak valid atau tidak lengkap.")
-    except Exception as e:
-        print(f"Terjadi kesalahan: {e}")
-"""
 def jelaskan_cron():
     try:
         with open('/etc/crontab', 'r') as file:
