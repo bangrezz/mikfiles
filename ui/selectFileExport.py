2f3d33afa437bf1197d1524e5375efebd8851d14
bangrezz <rezaekafebrian40@gmail.com>
2023-12-14 09:21:15 +0800

    changes UI

--- ui/selectFileExport.py
+++ ui/selectFileExport.py
@@ -4,13 +4,10 @@ Select to Export file
             
             
 (1) Export log file
-
 (2) Export file configuration and backup file
-
 (3) Export both
-
+(4) Automatic Export with Cron
 (b) Back to the previous
-
 (q) Exit the program
 
 """)
