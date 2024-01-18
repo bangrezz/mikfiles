3a909f3b65fb596c44cb6dfa5da0e59970737d92
bangrezz <rezaekafebrian40@gmail.com>
2024-01-18 11:43:31 +0800

    upload new development files

---
+++ modules/ipAddressLogDev.py
@@ -0,0 +1,136 @@
+"""
+[NOTE] :
+1. Ini adalah file development untuk 6 modul terbaru. Jika ini berhasil maka langsung di
+terapkan ke modul lainnya
+2. Modul ini versi terbarunya dapat menerima input port lebih dari 1 lalu mencobanya dengan
+input ip address dari user. Jika suatu kombinasi ip dan port berhasil login, maka skrip
+tidak akan mencoba login kembali ke ip yang sama dengan port berbeda
+3. Next coba buatkan user input counter pada bagian port input dan ip input. Jika input
+tidak sesuai format, maka skrip akan memberikan pesan error dan meminta user menginputkan ulang.
+Jika ini berhasil, cobalah untuk terapkan di modul cronjob.py
+4. Jika no 3 sudah berhasil, maka kembali ke no 1.
+5. Jika sudah melaksanakan no 4, maka perbaiki modul cronjob.py pada bagian regexp.
+Untuk mengupdate regexp search ke ip_input & port terbaru
+6. [Opsional] Jika nomor 5 berhasil. cobalah buat user input counter pada bagian cron.
+Cara kerjanya seperti nomor 3
+7. Batas akhir pengerjaan 31 januari 2024. Jika ada yang kurang/bingung maka stop development.
+Kemudian buatkan file readme untuk deskripsi mikfiles dan buat release point v1.0.0-stable
+"""
+import paramiko
+import ipaddress
+import socket
+import os
+import datetime
+import time
+import re
+
+def login_mikrotik(ip, username, password, port, login_status):
+    if login_status.get(ip, False):
+        print("\033[32m" + f"[i] {ip} already logged in, skipping..." + "\033[0m")
+        return False
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(ip, port=port, username=username, password=password, timeout=3)
+        print("\033[32m" + f"[i] Successfull {ip}:{port}" + "\033[0m")
+
+        login_status[ip] = True
+
+        # Get the hostname
+        stdin, stdout, stderr = ssh.exec_command(":put [/system identity get name]")
+        hostname = stdout.read().decode().strip()
+
+        # Get the current date and time
+        now = datetime.datetime.now()
+        date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+        # Define the log filename
+        log_filename = f"{ip}_{date_time}"
+
+        # Export the log file
+        command = f"/log print file={log_filename}"
+        stdin, stdout, stderr = ssh.exec_command(command)
+        print(f"[i] Execute command '{command}' di {ip}:")
+        print(stdout.read().decode())
+
+        time.sleep(5)  # wait for 5 seconds
+
+        # Buat objek SFTP
+        sftp = ssh.open_sftp()
+
+        # Pola regexp untuk mencocokkan nama file
+        # \S+ cocok dengan satu atau lebih karakter non-whitespace
+        pattern = rf"{ip}_\d{{4}}-\d{{2}}-\d{{2}}_\d{{2}}-\d{{2}}-\d{{2}}\.txt"
+
+        # Mendapatkan daftar semua file di direktori root
+        files = sftp.listdir('/')
+
+        # Mencari file yang cocok dengan pola
+        for file in files:
+            if re.match(pattern, file):
+                # Lokasi file di komputer lokal (direktori tempat skrip dijalankan)
+                local_file = os.path.join(os.getcwd(), file)
+
+                # Mendownload file
+                sftp.get('/' + file, local_file)
+
+                # deleteBackupFile
+                sftp.remove('/' + file)
+
+        # Menutup koneksi
+        sftp.close()
+        return True
+    except socket.timeout:
+        print("\033[31m" + f"[!] Target {ip}:{port} none: Connection timed out." + "\033[0m")
+        print("[i] Maybe devices is off or filtered by firewall devices")
+    except paramiko.AuthenticationException:
+        print(f"\033[31m" + "[!]" + "\033[0m" + f" Failed login to {ip}:{port} authentication failed")
+    except Exception as e:
+        print(f"\033[31m" + "[!]" + "\033[0m" + f" Failed login to {ip}:{port}: {e}")
+        return False
+    finally:
+        ssh.close()
+            
+def main():
+    username = str(input("[+] Input Username : "))
+    password = str(input("[+] Input Password : "))
+    ports_input = input("[+] Input SSH ports (comma separated) [Press Enter if default (port 22)] : ")
+    ports = [int(port.strip()) for port in ports_input.split(',')] if ports_input else [22]
+    
+    login_status = {}
+
+    def AddrInput():
+        ip_input = input("[+] input IP Address : ")
+
+        # Memisahkan input berdasarkan koma dan menghapus spasi
+        ip_entries = [ip.strip() for ip in ip_input.split(',')]
+        for entry in ip_entries:
+            if '-' in entry:
+                start_ip, end_ip = entry.split('-')
+                start_ip = ipaddress.IPv4Address(start_ip)
+                end_ip = ipaddress.IPv4Address(end_ip)
+
+                for ip_int in range(int(start_ip), int(end_ip) + 1):
+                    ip = ipaddress.IPv4Address(ip_int)
+                    for port in ports:
+                        if login_mikrotik(str(ip), username, password, port, login_status):
+                            break
+            elif '/' in entry:
+                ip_range = ipaddress.ip_network(entry, strict=False)
+                for ip in ip_range.hosts():
+                    for port in ports:
+                        if login_mikrotik(str(ip), username, password, port, login_status):
+                            break
+            else:
+                try:
+                    ipaddress.IPv4Address(entry)  # Memvalidasi alamat IP
+                    for port in ports:
+                        if login_mikrotik(entry, username, password, port, login_status):
+                            break
+                except ipaddress.AddressValueError as ave:
+                    print(f"\033[31m" + "[!]" + "\033[0m" + f" IP Address doesn't valid: {ave}")
+
+                    AddrInput()
+    AddrInput()
+main()
